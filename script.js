fc12a55f1d23f94fd5f4ecb2d24c5e9c699748f3
Kyle James <140095432+kylejames1998@users.noreply.github.com>
2023-11-01 20:36:27 -0700

    added some content

---
+++ script.js
@@ -0,0 +1,22 @@
+let myGrade = 67;
+if (myGrade >= 90) {
+    console.log("I got an A!");
+}
+else if (myGrade <90 && myGrade > 80) {
+    console.log("I got a B");
+}
+else if (myGrade < 80 && myGrade > 70) {
+    console.log("I got a C");
+}
+else if (myGrade < 70 && myGrade > 60) {
+    console.log("I got a D")
+}
+else if (myGrade < 60 && myGrade <50) {
+    console.log("I got an F");
+}
+
+switch (myGrade) {
+    case myGrade >= 90: console.log("A") 
+                        break;
+    case myGrade >= 80: console.log("B")
+}
